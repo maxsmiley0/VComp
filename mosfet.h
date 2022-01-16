75654fffdf581135eb4aacecdc5dbf6afbbfa208
Max Smiley <maxsmiley0@gmail.com>
2022-01-16 01:16:29 -0800

    Documented mosfet.h

--- mosfet.h
+++ mosfet.h
@@ -1,17 +1,19 @@
 #include <stdbool.h>
-#include <stdio.h>
 
+//Can either be PMOS or NMOS depending on initialization
 typedef struct {
     bool high;
     void* prev;
     void* next;
 } mosfet;
 
+//Voltage source for our transistors to pull from
 typedef struct {
     bool high;
     void* next;
 } vcc;
 
+//Specifies behavior upon wire crossing
 typedef struct {
     bool high;
     void* prev_1;
@@ -19,6 +21,7 @@ typedef struct {
     void* next;
 } juncture;
 
+//NMOS - essentially stores the voltage value given (as field high)
 void init_nmos(mosfet* m, bool h, void* p, void* n)
 {
     m->prev = p; 
@@ -26,6 +29,7 @@ void init_nmos(mosfet* m, bool h, void* p, void* n)
     m->next = n;
 }
 
+//PMOS - essentially inverts the voltage value given (as field high)
 void init_pmos(mosfet* m, bool h, void* p, void* n)
 {
     m->prev = p; 
@@ -33,12 +37,14 @@ void init_pmos(mosfet* m, bool h, void* p, void* n)
     m->next = n;
 }
 
+//Initializes voltage source as high
 void init_vcc(vcc* v, void* n)
 {
     v->high = true;
     v->next = n;
 }
 
+//Given two input wires, specifies the voltage value
 void init_juncture(juncture* j, void* p1, void* p2, void* n)
 {
     j->prev_1 = p1;
