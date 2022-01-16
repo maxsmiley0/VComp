9f19ef86710e18c60139f8076779976fb20ba41b
Max Smiley <maxsmiley0@gmail.com>
2022-01-16 02:38:58 -0800

    Completed standard logic gates, and added scheme to update all gates in a network in case of initial logic gate updates

--- gates.h
+++ gates.h
@@ -5,6 +5,7 @@
 typedef struct gate gate;
 struct gate {
     bool val;
+    char* type;
     gate* input_1;
     gate* input_2;
 };
@@ -13,6 +14,7 @@ struct gate {
 void init_gate(gate* g, bool val)
 {
     g->val = val;
+    g->type = "node";
     g->input_1 = NULL;
     g->input_2 = NULL;
 }
@@ -29,6 +31,7 @@ void init_and(gate* g, gate* i_1, gate* i_2)
     init_nmos(&second, i_2->val, &first);
 
     g->val = second.high;
+    g->type = "and";
     g->input_1 = i_1;
     g->input_2 = i_2;
 }
@@ -47,6 +50,7 @@ void init_or(gate* g, gate* i_1, gate* i_2)
     init_juncture(&j, &first, &second);
 
     g->val = j.high;
+    g->type = "or";
     g->input_1 = i_1;
     g->input_2 = i_2;
 }
@@ -60,6 +64,7 @@ void init_not(gate* g, gate* i)
     init_pmos(&first, i->val, &v);
 
     g->val = first.high;
+    g->type = "not";
     g->input_1 = i;
     g->input_2 = NULL;
 }
@@ -78,6 +83,7 @@ void init_nand(gate* g, gate* i_1, gate* i_2)
     init_juncture(&j, &first, &second);
 
     g->val = j.high;
+    g->type = "nand";
     g->input_1 = i_1;
     g->input_2 = i_2;
 }
@@ -94,18 +100,99 @@ void init_nor(gate* g, gate* i_1, gate* i_2)
     init_pmos(&second, i_2->val, &first);
 
     g->val = second.high;
+    g->type = "nor";
     g->input_1 = i_1;
     g->input_2 = i_2;
 }
 
-void init_xor()
+//Logical xor of two bits using mosfet
+void init_xor(gate* g, gate* i_1, gate* i_2)
 {
-    
+    vcc v;
+    mosfet up_left;
+    mosfet up_right;
+    mosfet down_left;
+    mosfet down_right;
+    juncture j;
+
+    init_vcc(&v);
+    init_nmos(&up_left, i_1->val, &v);
+    init_pmos(&up_right, i_2->val, &up_left);
+    init_pmos(&down_left, i_1->val, &v);
+    init_nmos(&down_right, i_2->val, &down_left);
+    init_juncture(&j, &up_right, &down_right);
+
+    g->val = j.high;
+    g->type = "xor";
+    g->input_1 = i_1;
+    g->input_2 = i_2;
 }
 
-void init_eql()
+//Logical eqv of two bits using mosfet
+void init_eqv(gate* g, gate* i_1, gate* i_2)
 {
-    
+    vcc v;
+    mosfet up_left;
+    mosfet up_right;
+    mosfet down_left;
+    mosfet down_right;
+    juncture j;
+
+    init_vcc(&v);
+    init_nmos(&up_left, i_1->val, &v);
+    init_nmos(&up_right, i_2->val, &up_left);
+    init_pmos(&down_left, i_1->val, &v);
+    init_pmos(&down_right, i_2->val, &down_left);
+    init_juncture(&j, &up_right, &down_right);
+
+    g->val = j.high;
+    g->type = "eqv";
+    g->input_1 = i_1;
+    g->input_2 = i_2;
 }
 
-//Something to update a network
+//Given a logic gate, updates all values of gates previous (and including self) in case of an update somewhere
+void update_network(gate* g)
+{
+    if (g->type == "and")
+    {
+        update_network(g->input_1);
+        update_network(g->input_2);
+        init_and(g, g->input_1, g->input_2);
+    }
+    else if (g->type == "or")
+    {
+        update_network(g->input_1);
+        update_network(g->input_2);
+        init_or(g, g->input_1, g->input_2);
+    }
+    else if (g->type == "not")
+    {
+        update_network(g->input_1);
+        init_not(g, g->input_1);
+    }
+    else if (g->type == "nand")
+    {
+        update_network(g->input_1);
+        update_network(g->input_2);
+        init_nand(g, g->input_1, g->input_2);
+    }
+    else if (g->type == "nor")
+    {
+        update_network(g->input_1);
+        update_network(g->input_2);
+        init_nor(g, g->input_1, g->input_2);
+    }
+    else if (g->type == "xor")
+    {
+        update_network(g->input_1);
+        update_network(g->input_2);
+        init_xor(g, g->input_1, g->input_2);
+    }
+    else if (g->type == "eqv")
+    {
+        update_network(g->input_1);
+        update_network(g->input_2);
+        init_eqv(g, g->input_1, g->input_2);
+    }
+}
--- main.c
+++ main.c
@@ -3,33 +3,15 @@
 
 int main()
 {
-    /*
-    //inverter
-    vcc s;
-    mosfet n;
-    init_vcc(&s);
-    init_pmos(&n, x, &s);
-
-    //xor (x and not y) or (not x and y)
-    vcc o1;
-    vcc o2;
-    mosfet r1;
-    mosfet r2;
-    mosfet r3;
-    mosfet r4;
-    juncture jp;
-    init_vcc(&o1);
-    init_nmos(&r1, x, &o1);
-    init_pmos(&r2, y, &r1);
-    init_pmos(&r3, x, &o1);
-    init_nmos(&r4, y, &r3);
-    init_juncture(&jp, &r2, &r4);
-
-*/
-    gate x, y, g;
+    gate x, y, z, f, g;
     init_gate(&x, false);
     init_gate(&y, true);
+    init_gate(&z, false);
 
-    init_nand(&g, &x, &y);
+    init_xor(&f, &x, &y);
+    init_eqv(&g, &f, &z);
+
+    x.val = true;
+    update_network(&g);
     printf("%d\n", g.val);
 }
